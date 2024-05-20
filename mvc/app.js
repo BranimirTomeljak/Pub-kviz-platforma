07060b92b450412c92986b518b34aec3295b4a23
BranimirTomeljak <mtomeljak@gmail.com>
2024-05-20 16:17:41 +0200

    minor changes

--- mvc/app.js
+++ mvc/app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./models");
 const indexRouter = require("./routes/index");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +13,16 @@ app.use(bodyParser.json());
 
 app.use("/", indexRouter);
 
-const PORT = process.env.PORT || 3000;
+app.use((req, res, next) => {
+  res.status(404).render("error");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+
 
 db.sequelize
   .sync()
--- mvc/controllers/userController.js
+++ mvc/controllers/userController.js
@@ -1,9 +1,10 @@
-const db = require("../models");
+const db = require('../models');
+const logger = require('../config/logger');
 
 exports.getUsers = async (req, res) => {
   try {
     const users = await db.Korisnik.findAll();
-    res.render("users", { users });
+    res.render('users', { users });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -13,6 +14,7 @@ exports.createUser = async (req, res) => {
   try {
     const { id, email } = req.body;
     const newUser = await db.Korisnik.create({ id, email });
+    logger.info(`User created: ${newUser.email}`);
     res.status(201).json(newUser);
   } catch (error) {
     res.status(500).send(error.message);
