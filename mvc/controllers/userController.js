96b9a77ed9f560c1e21332d23e111d56fc8c79a2
BranimirTomeljak <mtomeljak@gmail.com>
2024-05-26 16:01:04 +0200

    user is organizator

--- mvc/controllers/userController.js
+++ mvc/controllers/userController.js
@@ -1,10 +1,11 @@
 const db = require("../models");
 const logger = require("../config/logger");
 
-exports.createUser = async (req, res) => { // dodaj i da je organizator automatski
+exports.createUser = async (req, res) => {
   try {
     const { email } = req.body;
     const newUser = await db.Korisnik.create({ email });
+    await db.Organizator.create({ id: newUser.id });
     logger.info(`User created: ${newUser.email}`);
     res.status(201).json(newUser);
   } catch (error) {
--- mvc/routes/userRoutes.js
+++ mvc/routes/userRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
+router.post("/create", userController.createUser);
 router.get("/znj/:email", userController.getUser);
 router.get("/", userController.getUsers);
-router.post("/", userController.createUser);
 
 module.exports = router;
